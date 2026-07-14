5862ed8d54efc263358485f1b6b695aad5277b70
Mitchell Mashburn <128167557+re3Dev@users.noreply.github.com>
2026-07-14 11:00:41 -0500

    Make agitator hall input normally low

--- Production_bundle/LunaRecycle/LunaRecycle.ino
+++ Production_bundle/LunaRecycle/LunaRecycle.ino
@@ -133,7 +133,7 @@ const int Mixer_screwRotationSensor   = 2;    // D2 = INT0
 const int Mixer_agitatorMotor_ENB = 11;    // ENB enable (PWM)
 const int Mixer_agitatorMotor_IN3 = 42;   // IN3
 const int Mixer_agitatorMotor_IN4 = 43;   // IN4
-const int Mixer_agitatorHallSensor = 50;  // Hall home sensor input (INPUT_PULLUP)
+const int Mixer_agitatorHallSensor = 50;  // Hall home sensor input (normally low; external pulldown)
 
 // Trash conveyor (pinmap_mega2560.md).
 const int TC_stepperMotorController_step   = 4;
@@ -2525,7 +2525,7 @@ void setup() {
   pinMode(Mixer_agitatorMotor_ENB, OUTPUT);
   pinMode(Mixer_agitatorMotor_IN3, OUTPUT);
   pinMode(Mixer_agitatorMotor_IN4, OUTPUT);
-  pinMode(Mixer_agitatorHallSensor, INPUT_PULLUP);
+  pinMode(Mixer_agitatorHallSensor, INPUT);
   agitatorStop();
 
   // Mixer blast gates - hold RC neutral (1500 us) so the RoboClaw arms;

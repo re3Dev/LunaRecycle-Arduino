f07fdf206a9454b75c571d23def0ae5f30960628
Mitchell Mashburn <128167557+re3Dev@users.noreply.github.com>
2026-07-22 13:33:44 -0500

    Change bag drop positioning

--- Production_bundle/LunaRecycle/LunaRecycle.ino
+++ Production_bundle/LunaRecycle/LunaRecycle.ino
@@ -299,8 +299,8 @@ const int   TC_microsteps = 4;
 const float TC_homePos       = 0.0;
 const float TC_bag1Pos       = -25.0;
 const float TC_bag2Pos       = -565.0;   // Tune to the Bag 2 stack position.
-const float TC_bag1ShredderPos = -475.0;
-const float TC_bag2ShredderPos = -295.0;   // 20mm farther right than -275.0.
+const float TC_bag1ShredderPos = -450.0;
+const float TC_bag2ShredderPos = -290.0;   // 20mm farther right than -275.0.
 const float TC_stepperMinPos = -570.0;
 const float TC_stepperMaxPos = -10.0;
 
